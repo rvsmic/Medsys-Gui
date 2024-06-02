1553885bb97db702a1f68b0ae0bf31d591083203
Michał Rusinek <307349@office.umcs.pl>
2024-06-02 19:25:23 +0200

    Finished visits page

--- src/api/visitsApi.js
+++ src/api/visitsApi.js
@@ -1,7 +1,31 @@
 import { apiConnection } from './apiConnection'
 
 export default {
-  getVisits() {
-    return apiConnection.get('/visits', { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } })
+  getVisits(token, id=null) {
+    return apiConnection.get('/service/visits', { 
+      params: { id: id },
+      headers: { token: token }
+    })
+  },
+  postVisit(token, visit) {
+    return apiConnection.post('/service/visits', { 
+      params: { visit: visit },
+      headers: { token: token }
+    })
+  },
+  patchVisit(token, id, visit) {
+    return apiConnection.patch('/service/visits', { 
+      params: {
+        id: id,
+        visit: visit
+      },
+      headers: { token: token }
+    })
+  },
+  deleteVisit(token, id) {
+    return apiConnection.delete('/service/visits', { 
+      params: { id: id },
+      headers: { token: token }
+    })
   }
 }
